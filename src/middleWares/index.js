a5676ec4f7bb2d4a352a3bccecdddea9ec2769c9
danil <minakonn9shkoni@gmail.com>
2022-06-03 17:05:10 +0300

    add correct

--- src/middleWares/index.js
+++ src/middleWares/index.js
@@ -1,7 +1,7 @@
 module.exports.middleWare = (req, res, next) => {
   const verdict = !isNan(Number(req.params.id));
   if (verdict) {
-    next();
+    return next();
   }
   res.status(400).send('Error');
 };
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,6 @@
 const getHome = (req, res) => {
   console.log(req.params.id);
-  res.status(200).send(id:params.id);
+  res.status(200).send({id:req.params.id});
 };
 //module.exports = getHome;
 module.exports.getHome = getHome;
