d5d5d6755b6727a90e0d0fa20e96fc281094998e
danil <minakonn9shkoni@gmail.com>
2022-06-03 17:09:38 +0300

    add yup for example

--- src/middleWares/index.js
+++ src/middleWares/index.js
@@ -1,5 +1,8 @@
+const yup = require('yup');
+const paramId = yup.number().positive().required();
+
 module.exports.middleWare = (req, res, next) => {
-  const verdict = !isNan(Number(req.params.id));
+  const verdict = paramId.isValidSync(req.params.id);
   if (verdict) {
     return next();
   }
