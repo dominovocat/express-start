74ca84ee8fb656cf9e450e1869b3667ec444b07e
danil <minakonn9shkoni@gmail.com>
2022-06-09 17:11:56 +0300

    Add small example

--- fd-db.session.sql
+++ fd-db.session.sql
@@ -16,3 +16,19 @@ VALUES (200, 'tets', 'test2', true);
 --@block
 INSERT INTO "users" (firstName, lastName, isMale)
 VALUES ('test', 'test2', true);
+
+
+--@block
+CREATE TABLE IF NOT EXISTS "products"(
+  id serial UNIQUE NOT NULL,
+  name varchar(15) NOT NULL CHECK(char_length (name)>=3),
+  price DECIMAL(7,2) NOT NULL 
+  DEFAULT 0.00 CONSTRAINT "positive_price" CHECK(price >= 0.00),
+  quantity DECIMAL (5,0) NOT NULL DEFAULT 0 
+);
+
+--@block
+INSERT INTO "products" (name,price) VALUES (
+  'banana',
+  19.50
+);
